6462a779cce623d55621a735eeffe52002e9d556
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-18 15:32:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const makeChange = (c) => {
+function makeChange(amount){
+q=25;//quarter
+	d=10;//dime
+	n=5//nickel
+	p=1;//penny
+}
+const result={
+	q:0,
+	d:0,
+	n:0,
+	p:0,
+};
+	for(const coin in coins){
+		while (amount>=coins[coin]) {
+			amount-=coins[coin];
+			result[coin]++;
+		}
+	}
+	return result; 
   // your name here
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
