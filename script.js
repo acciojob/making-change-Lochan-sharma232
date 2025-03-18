4125fc4b1e227d9e658f2eb586a28b67862c8e54
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-18 15:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ const result={
 };
 
 // Do not the change the code below
-// const c = prompt("Enter c: ");
-// alert(JSON.stringify(makeChange(c)));
+const c = prompt("Enter c: ");
+alert(JSON.stringify(makeChange(c)));
